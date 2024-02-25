49f4476a72539bb19546a279fc1fe073cc9f7b21
sk2001git <122198303+sk2001git@users.noreply.github.com>
2024-02-25 22:08:57 +0800

    Signout attempt fix 6

--- components/auth/SignOutButton.tsx
+++ components/auth/SignOutButton.tsx
@@ -1,37 +1,15 @@
 "use client";
-import { signOut } from "@/auth";
-import { revalidatePath } from "next/cache";
-import { useRouter } from "next/navigation";
+import { logout } from "@/lib/actions/logout";
+
 
 
 export const SignOutButton: React.FC = () => {
-  // const onClick = async () => {
-  //   await signOut();
-  //   revalidatePath("/");
-  // }
+ 
+  const onClick = () => {
+    logout();
+  }
 
-  const handleSignOut = async () => {
-    try {
-      const response = await fetch('/api/auth/signout', {
-        method: 'POST', // or 'GET' depending on your server implementation
-        headers: {
-          'Content-Type': 'application/json',
-          // Add any additional headers if needed
-        },
-      });
-  
-      if (response.ok) {
-        // Optionally, perform additional client-side actions after successful sign-out
-        console.log('Sign-out successful');
-      } else {
-        console.error('Sign-out failed');
-      }
-    } catch (error) {
-      console.error('Error during sign-out:', error);
-    }
-  };
-
-  return (<span onClick={handleSignOut} className="cursor-pointer nav-words">
+  return (<span onClick={onClick} className="cursor-pointer nav-words">
     Sign Out
   </span>
   )
