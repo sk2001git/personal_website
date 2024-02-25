a6c7ddb1c709305dbe0a371d5d5f9cba1db0df20
sk2001git <122198303+sk2001git@users.noreply.github.com>
2024-02-25 19:50:08 +0800

    Try Sign out naive approach fix

--- components/auth/SignOutButton.tsx
+++ components/auth/SignOutButton.tsx
@@ -2,7 +2,8 @@ import { signOut } from "next-auth/react";
 
 export const SignOutButton: React.FC = () => {
   const onClick = async () => {
-    await signOut();
+    "use server";
+    await signOut({callbackUrl: "/api/auth/signout"});
   }
   return (<span onClick={onClick} className="cursor-pointer nav-words">
     Sign Out
