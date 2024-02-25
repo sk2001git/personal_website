d76de2295c91a8410aa24fa7bb73af5ba7260cc9
sk2001git <122198303+sk2001git@users.noreply.github.com>
2024-02-25 20:26:15 +0800

    Signout attempt fix 4

--- components/auth/SignOutButton.tsx
+++ components/auth/SignOutButton.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { signOut } from "@/auth";
+import { revalidatePath } from "next/cache";
 import { useRouter } from "next/navigation";
 
 
 export const SignOutButton: React.FC = () => {
   const onClick = async () => {
     await signOut();
+    revalidatePath("/");
   }
   return (<span onClick={onClick} className="cursor-pointer nav-words">
     Sign Out
