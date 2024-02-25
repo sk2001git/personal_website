3f03c9ed1a9feef01ae70635b7c76773a884965b
sk2001git <122198303+sk2001git@users.noreply.github.com>
2024-02-25 22:34:51 +0800

    Signout attempt fix 7

--- components/auth/SignOutButton.tsx
+++ components/auth/SignOutButton.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { logout } from "@/lib/actions/logout";
+import { useSession } from "next-auth/react";
 
 
 
 export const SignOutButton: React.FC = () => {
- 
+  const session =  useSession();
   const onClick = () => {
     logout();
+    session.update(); 
   }
 
   return (<span onClick={onClick} className="cursor-pointer nav-words">
--- lib/actions/logout.ts
+++ lib/actions/logout.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { signOut } from "@/auth";
+import { signOut } from "next-auth/react";
 
 export const logout = async () => {
   await signOut();
